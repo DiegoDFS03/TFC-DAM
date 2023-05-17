e9158a691e6c177ba8533a507d0ff39523497084
DiegoDFS03 <diegodfs08082003@gmail.com>
2023-05-17 19:57:57 +0200

    Models creados

--- GameSaverX/rest/GameSaverX/rest_api/admin.py
+++ GameSaverX/rest/GameSaverX/rest_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User,UserOffer,Store,Offer
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Offer)
+admin.site.register(Store)
+admin.site.register(UserOffer)
--- GameSaverX/rest/GameSaverX/rest_api/models.py
+++ GameSaverX/rest/GameSaverX/rest_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    surnames = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=150)
+    token = models.CharField(max_length=20, unique=True, null=True)
+    password_token = models.CharField(max_length=20, null=True)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=40)
+    logo = models.CharField(max_length=50)
+
+
+class Offer(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=50)
+    original_price = models.DecimalField(max_digits=8, decimal_places=2)
+    genre = models.CharField(max_length=50)
+    release_date = models.DateField()
+    developer = models.CharField(max_length=70)
+    publisher = models.CharField(max_length=70)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    discount_percentage = models.DecimalField(max_digits=5,decimal_places=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class UserOffer(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
