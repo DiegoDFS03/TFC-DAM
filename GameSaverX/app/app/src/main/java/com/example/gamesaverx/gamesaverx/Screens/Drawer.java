56eb0a868f699f9db4fca4770d1532a81780f82a
DiegoDFS03 <diegodfs08082003@gmail.com>
2023-05-30 20:39:33 +0200

    Drawer completado

---
+++ GameSaverX/app/app/src/main/java/com/example/gamesaverx/gamesaverx/Screens/Drawer.java
@@ -0,0 +1,21 @@
+package com.example.gamesaverx.gamesaverx.Screens;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.gamesaverx.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+
+    }
+}
